fe096e06cec7b02e57bbaa355069fe4c9c96c2e8
Jose Carrizales <jrafaelca@gmail.com>
2025-02-07 14:45:23 -0300

    feat: add default layout and update app structure with Nuxt components

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,12 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtLoadingIndicator/>
+    <NuxtLayout>
+      <NuxtPage/>
+    </NuxtLayout>
   </div>
 </template>
