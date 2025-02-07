4692e4c7961f42cca9852c2496b45abe45985c67
Jose Carrizales <jrafaelca@gmail.com>
2025-02-07 15:17:25 -0300

    feat: add locale support (#3)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,17 @@
 export default defineNuxtConfig({
-    modules: ['@nuxt/ui'],
+    modules: ['@nuxt/ui', '@nuxtjs/i18n'],
     css: ['~/assets/css/main.css'],
     future: {
         compatibilityVersion: 4,
     },
     compatibilityDate: '2024-11-01',
-    devtools: {enabled: true}
+    devtools: {enabled: true},
+    i18n: {
+        locales: [
+            {code: 'en', file: 'en.json'},
+            {code: 'es', file: 'es.json'}
+        ],
+        lazy: true,
+        defaultLocale: process.env.APP_LOCALE || 'en',
+    },
 })
