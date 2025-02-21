47ffcc091aefa9231f99cdeeb20e1443c5466f38
Jose Carrizales <jrafaelca@gmail.com>
2025-02-21 18:17:37 -0300

    feat(config): integrate ESLint with Nuxt setup (#3)
    - Added ESLint integration using the `@nuxt/eslint` module for improved code quality and consistent linting.
    - Configured ESLint with development-specific options and included a custom ESLint configuration file.
    - Updated dependencies to incorporate required ESLint and related packages.

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,13 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import { defineNuxtConfig } from 'nuxt/config'
+
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['@nuxt/eslint'],
+  eslint: {
+    config: {
+      stylistic: true,
+      autoInit: process.env.NODE_ENV === 'development',
+    }
+  }
 })
